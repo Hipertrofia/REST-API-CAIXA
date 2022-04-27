52feecf69fd99bd88af53a7987a88b233eaa7483
Hipertrofia <jansen@dcc.ufrj.br>
2022-04-27 19:54:42 -0300

    Ajustando os métodos de contas

--- routes/contas.js
+++ routes/contas.js
@@ -20,7 +20,7 @@ router.get('/', (req, res, next) =>{
                             saldo: cont.saldo,
                             request: {
                                 tipo: 'GET',
-                                descricao: 'Retorna uma lista com todas as contas',
+                                descricao: 'Retorna os detalhes de uma conta especifica',
                                 url:'http://localhost:3000/contas/'+cont.id_contas
 
                             }
@@ -41,9 +41,28 @@ router.get('/:id_contas', (req, res, next) =>{
         conn.query(
             'SELECT * FROM contas WHERE id_contas =?;',
             [req.params.id_contas],
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({error: error})}
-                return res.status(200).send({response: resultado})
+                
+                if(result.length== 0){
+                    return res.status(404).send({
+                        mensagem:'Não encontramos uma conta com este ID'
+                    })
+                }
+                
+                const response={
+                    conta: {
+                        id_contas:result[0].id_contas,
+                        proprietario:result[0].proprietario,
+                        saldo: result[0].saldo,
+                        request:{
+                            tipo:'GET',
+                            descricao:'Retorna todas as contas ',
+                            url:'http://localhost:3000/contas'
+                        }
+                    }
+                }
+                return res.status(201).send(response);
             }
         )
     })
@@ -58,16 +77,25 @@ router.post('/', (req, res, next) => {
         conn.query(
             'INSERT INTO contas (proprietario, saldo) VALUES (?,?)',
             [req.body.proprietario, req.body.saldo], 
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 conn.release();
                 if (error){ return res.status(500).send({error: error})}   
-
-                res.status(201).send({
-                    mensagem: 'Conta aberta com sucesso!',
-                    id_contas: resultado.insertId
-                });
+                const response={
+                    mensagem:'Conta aberta com sucesso',
+                    contaAberta: {
+                        id_contas:result.id_contas,
+                        proprietario:req.body.proprietario,
+                        saldo: req.body.saldo,
+                        request:{
+                            tipo:'POST',
+                            descricao:'Abre uma conta',
+                            url:'http://localhost:3000/contas'
+                        }
+                    }
+                }
+                return res.status(201).send(response);
             }
-        )
+        ) 
     })  
 });
 
@@ -85,13 +113,23 @@ router.patch('/', (req, res, next) => {
                 req.body.saldo,
                 req.body.id_contas
             ], 
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 conn.release();
                 if (error){ return res.status(500).send({error: error})}   
-
-                res.status(202).send({
-                    mensagem: 'Os dados da conta foram atualizados'
-                });
+                const response={
+                    mensagem:'Conta atualizada com sucesso',
+                    contaAtualizada: {
+                        id_contas:req.body.id_contas,
+                        proprietario:req.body.proprietario,
+                        saldo: req.body.saldo,
+                        request:{
+                            tipo:'PATCH',
+                            descricao:'Retorna os detalhes de uma determinada conta',
+                            url:'http://localhost:3000/contas/'+req.body.id_contas
+                        }
+                    }
+                }
+                return res.status(202).send(response);
             }
         )
     })
@@ -102,13 +140,22 @@ router.delete('/', (req, res, next) =>{
         if (error) { return res.status(500).send({error: error})}
         conn.query(
             `DELETE FROM contas WHERE id_contas = ?`, [req.body.id_contas], 
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 conn.release();
                 if (error){ return res.status(500).send({error: error})}   
-
-                res.status(202).send({
-                    mensagem: 'A conta foi fechada'
-                });
+				const response={
+					mensagem:'A Conta foi com sucesso',
+					request: {
+						tipo:'POST',
+						descricao: 'Insere um produto',
+						url:'http://localhost:3000/contas/',
+						body:{
+							proprietario:'String',
+							saldo:'Number'
+						}
+					}
+				}
+                return res.status(202).send(response);
             }
         )
     })
